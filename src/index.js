021848dcfa5c80d74b3d71a98b9572b47e7ad7e6
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-05-26 11:12:09 -0400

    winner declaration logic added

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ function Square(props){
 
     handleClick(i){
           const squares = this.state.squares.slice();
+          if (declarewinner(this.state.squares) || squares[i]){
+              return;
+          }
           squares[i] = this.state.xIsNext ? 'X' : 'O';
           this.setState({
               squares: squares,
@@ -36,8 +39,13 @@ function Square(props){
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
+      const winner = declarewinner(this.state.squares)  
+      let status;
+      if (winner){
+          status = 'winner: ' + winner;
+      }else{
+          status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
       return (
         <div>
           <div className="status">{status}</div>
@@ -75,6 +83,27 @@ function Square(props){
         </div>
       );
     }
+  }
+
+  function declarewinner(squares){
+      const lines = [
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+          [0,3,6],
+          [1,4,7],
+          [2,5,8],
+          [0,4,8],
+          [2,4,6]
+      ];
+
+      for(let i=0; i<lines.length; i++){
+          const [a,b,c] = lines[i]
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+              return squares[a];
+          }
+      }
+      return null
   }
   
   ReactDOM.render(
